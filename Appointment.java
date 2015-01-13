78e1bcbbeacec9d01c720a1a27b18c06d8480101
Haripriya Pandya <hpandya2@hawk.iit.edu>
2015-01-12 18:44:47 -0800

    first commit

---
+++ Appointment.java
@@ -0,0 +1,72 @@
+import java.time.Duration;
+import java.util.Date;
+import java.util.Scanner;
+
+
+
+
+public class Appointment {
+
+	Duration time;
+	Date start_date;
+	Date end_date;
+	String description;
+	boolean reoccure;
+	
+	public Appointment(Duration time, Date startdate, Date enddate, boolean reoccur){
+		Scanner scan = new Scanner(System.in);
+		this.time = time;
+		this.start_date = startdate;
+		this.end_date = enddate;
+		this.reoccure = reoccur;
+		
+		System.out.print("Please enter a description for the appointment:");
+		this.description = scan.nextLine();
+	}
+
+	public Duration getTime() {
+		return time;
+	}
+
+	public void setTime(Duration time) {
+		this.time = time;
+	}
+
+	public Date getStart_date() {
+		return start_date;
+	}
+
+	public void setStart_date(Date start_date) {
+		this.start_date = start_date;
+	}
+
+	public Date getEnd_date() {
+		return end_date;
+	}
+
+	public void setEnd_date(Date end_date) {
+		this.end_date = end_date;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public boolean isReoccure() {
+		return reoccure;
+	}
+
+	public void setReoccure(boolean reoccure) {
+		this.reoccure = reoccure;
+	}
+
+	
+	
+	
+	
+	
+}
---
+++ Calendar.java
@@ -0,0 +1,15 @@
+import java.util.ArrayList;
+
+
+public class Calendar {
+
+	String name;
+	ArrayList<Appointment> appointments;
+	
+	public Calendar(String name){
+		
+		this.name = name;
+		ArrayList<Appointment> appointment;  
+	}
+	
+}
