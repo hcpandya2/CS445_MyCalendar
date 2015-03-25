51c24c33ee0e705d605fb517c10de0fe255d32e7
Haripriya Pandya <hpandya2@hawk.iit.edu>
2015-03-25 17:16:21 -0500

    changes t file_reader

--- File_Reader.java
+++ File_Reader.java
@@ -5,6 +5,7 @@ import java.io.IOException;
 
 
 
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.HashMap;
@@ -39,13 +40,50 @@ public class File_Reader {
 			int counter = 1;
 			for (Iterator i = cal.getComponents().iterator(); i.hasNext();) {
 			    Component component = (Component) i.next();
-			    //System.out.println("Component [" + component.getName() + "]");
-			    list_of_components.put(new Integer(counter),component);
-			    System.out.println(counter + "\n" + list_of_components.get(counter));
+			    
+			    if(component.getName().equalsIgnoreCase("VEVENT")){
+				    list_of_components.put(new Integer(counter),component);
+				    /*System.out.println(counter + "\n" + list_of_components.get(counter)); */
+				    
+				    SimpleDateFormat SDF = new SimpleDateFormat("yyyyMMdd'T'HHmmss");
+					
+				    Date start = SDF.parse(component.getProperty("DTSTART").getValue());
+			        Date end = SDF.parse(component.getProperty("DTEND").getValue());
+			        
+			        java.util.Calendar startdate = java.util.Calendar.getInstance();
+			        java.util.Calendar enddate = java.util.Calendar.getInstance();
+			        int startday = start.getDay();
+			        int endday = end.getDay();
+			        
+			        startdate.set(start.getYear(),start.getMonth(),start.getDate(),start.getHours(),start.getMinutes(), start.getSeconds());
+			        enddate.set(end.getYear(),end.getMonth(),end.getDate(),end.getHours(),end.getMinutes(), end.getSeconds());
+			        
+			        String title = component.getProperty("NAME").getValue();
+			        String summary = component.getProperty("SUMMARY").getValue();
+				    Date time = SDF.parse(component.getProperty("DTSTAMP").getValue());
+				   
+				   /* if(component.)
+				    String location = component.getProperty("LOCATION").getValue();
+				    */
+				    int duration = Integer.parseInt(component.getProperty("DURATION").getValue());
+				    
+				    String recurrence = component.getProperty("FREQ").getValue();
+			        
+				    String description = component.getProperty("DESCRIPTION").getValue();
+				    
+			        //setting up the appointment object with the given date.... 
+				    Appointment appointment = new Appointment(title,description,startdate,duration,recurrence);
+				    appointment.setEnd_date(enddate);
+				    appointment.setDescription(description);
+				    appointment.setDuration(duration);
+			    
+			    }
 			    counter++;
-			}
+			    
+			}  // end of for loop iterator over the file ... 
 			
 			
+
 			
 		}// end of try block
 		catch (FileNotFoundException e) {
